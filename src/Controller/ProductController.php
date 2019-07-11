fef20494ab071d479850b81392e1ad25c4510d7a
dangeol <do.dev@outlook.com>
2019-07-11 08:54:09 +0200

    experiments formulaire filtre

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,24 +1,46 @@
 <?php
-// src/Controller/BlogController.php
 namespace App\Controller;
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use App\Form\ProductFilterType;
 use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 
 {
-	/**
-	 * Matches /product exactly
-	 * @Route("/product", name="product_list")
-	 */
-	public function listProductAction()
+    /**
+     * @param Request $request
+     * @Route("/product", name="product_list")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+	public function listProductAction(Request $request)
 	{
+        $form = $this->get('form.factory')->create(ProductFilterType::class);
+
+        if ($request->query->has($form->getName())) {
+            // manually bind values from the request
+            $form->submit($request->query->get($form->getName()));
+
+            // initialize a query builder
+            $filterBuilder = $this->getDoctrine()->getManager()
+                ->getRepository(Product::class)
+                ->createQueryBuilder('e');
+
+            // build the query from the given form object
+            $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($form, $filterBuilder);
+
+            // now look at the DQL =)
+            var_dump($filterBuilder->getDql());
+        }
+
         $product = $this->getDoctrine()
             ->getRepository(Product::class)
             ->findAll();
 
-        return $this->render('product/list.html.twig', ['product' => $product]);
+        return $this->render('product/list.html.twig', array(
+            'form' => $form->createView(),
+            'product' => $product
+        ));
 	}
 
     /**
